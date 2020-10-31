7fa1decd10cb9e2f51bd0a5ee5cac0b98203b387
Ana Luiza <analuizaramalho9@gmail.com>
2020-10-31 13:07:03 -0300

    Criação do Lista Tarefas Fragment

--- ExercicioAula27/app/src/main/java/com/example/exercicioaula27/todolist/view/ListaTarefasFragment.kt
+++ ExercicioAula27/app/src/main/java/com/example/exercicioaula27/todolist/view/ListaTarefasFragment.kt
@@ -1,59 +1,47 @@
-package com.example.exercicioaula27
+package com.example.exercicioaula27.todolist.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.exercicioaula27.R
+import com.example.exercicioaula27.todolist.model.Todo
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListaTarefasFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListaTarefasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var viewLista:View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_tarefas, container, false)
+        viewLista = inflater.inflate(R.layout.fragment_lista_tarefas, container, false)
+
+        val viewManager= LinearLayoutManager(context)
+        val recyclerView =viewLista.findViewById<RecyclerView>(R.id.rvLista)
+        val viewAdapter = TarefaAdapter(arrayListOf(Todo("tarefa1",false), Todo("tarefa1",false)))
+
+        recyclerView.apply {
+            setHasFixedSize(true)//dizer que os elementos não são dinamicos
+
+            layoutManager = viewManager
+            adapter=viewAdapter
+            addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
+        }
+
+
+
+        return viewLista
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaTarefasFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListaTarefasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() =
+            ListaTarefasFragment()
     }
 }
